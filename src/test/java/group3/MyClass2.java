607b211d580eedff04e7aac5b4584c8b88c3c3f6
Ugur Nurcan <ugur-n@yandex.com>
2021-11-19 00:09:27 -0500

    local8

--- src/test/java/group3/MyClass2.java
+++ src/test/java/group3/MyClass2.java
@@ -2,6 +2,43 @@ package group3;
 
 public class MyClass2 {
     public static void main(String[] args) {
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");
 
         System.out.println("my first stash line");
         System.out.println("master second line");
