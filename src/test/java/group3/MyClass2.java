9fe18bbe06b41e7c51f69f46ea42c713e7007ba0
Ugur Nurcan <33135616+ugurnur@users.noreply.github.com>
2021-11-19 00:14:41 -0500

    Update MyClass2.java

--- src/test/java/group3/MyClass2.java
+++ src/test/java/group3/MyClass2.java
@@ -3,7 +3,7 @@ package group3;
 public class MyClass2 {
     public static void main(String[] args) {
 
-        System.out.println("my first stash line");
+        System.out.println("444444my first stash line");
         System.out.println("master second line");
         System.out.println("My local line");
         System.out.println("my first stash2 line");
