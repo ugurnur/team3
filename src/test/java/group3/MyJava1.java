d66de0b01b6e08088e2bf858d8150dd970f77298
Ugur Nurcan <ugur-n@yandex.com>
2021-11-18 23:51:26 -0500

    master 3

--- src/test/java/group3/MyJava1.java
+++ src/test/java/group3/MyJava1.java
@@ -3,11 +3,11 @@ package group3;
 public class MyJava1 {
 
     public static void main(String[] args) {
+        System.out.println("my second stash line");
+        System.out.println("my first stash line");
         System.out.println("My first master line");
         System.out.println("master second line");
         System.out.println("My local line");
-        System.out.println("my second stash line");
 
-        System.out.println("my first stash line");
     }
 }
