4489eaae9d078d056e037bedac5afc1f3c731f76
Ugur Nurcan <ugur-n@yandex.com>
2021-11-18 23:46:44 -0500

    master commit 2

--- src/test/java/group3/MyJava1.java
+++ src/test/java/group3/MyJava1.java
@@ -4,6 +4,7 @@ public class MyJava1 {
 
     public static void main(String[] args) {
         System.out.println("My first master line");
+        System.out.println("master second line");
         System.out.println("My local line");
     }
 }
