bad236ab3ad444c83514ea99b4bd33ff0f64d9c4
Ugur Nurcan <ugur-n@yandex.com>
2021-11-18 23:49:53 -0500

    local commit 2

--- src/test/java/group3/MyJava1.java
+++ src/test/java/group3/MyJava1.java
@@ -6,5 +6,8 @@ public class MyJava1 {
         System.out.println("My first master line");
         System.out.println("master second line");
         System.out.println("My local line");
+        System.out.println("my second stash line");
+
+        System.out.println("my first stash line");
     }
 }
