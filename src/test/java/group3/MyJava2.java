4f72582963e84a37d4c3e3c16a556c6b4d2c33ad
Ugur Nurcan <ugur-n@yandex.com>
2021-11-19 00:01:53 -0500

    local6

---
+++ src/test/java/group3/MyJava2.java
@@ -0,0 +1,12 @@
+package group3;
+
+public class MyJava2 {
+    public static void main(String[] args) {
+        System.out.println("11111111My local line");
+        System.out.println("my first stash2 line");
+        System.out.println("my first stash line");
+        System.out.println("my first stash line");
+        System.out.println("55555master second line");
+
+    }
+}
